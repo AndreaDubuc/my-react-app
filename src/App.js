6f4ae736bd7060d38a8522d4c8024542432c1b0e
Andrea Dubuc <dubuc.andrea@gmail.com>
2022-01-11 22:29:56 -0500

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ import './App.css';
 export default function App() {
   return (
 <div className="App">
-      Weather App
+      <h1> Weather App </h1>
+<footer> This is project was coded by <a href="https://www.instagram.com/_andreacodes/" target="_blank">  Andrea </a> and is open-source on {" "}
+
+      <a href="https://github.com/AndreaDubuc/my-react-app"
+      target="_blank">
+         GitHub
+        </a>
+        </footer>
     </div>
   );
 }
