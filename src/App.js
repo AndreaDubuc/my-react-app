2e13dd3b8cb066a4f4b17d95469606bcb3640d29
Andrea Dubuc <dubuc.andrea@gmail.com>
2022-01-11 22:48:31 -0500

    Added bootstrap to my project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
 import React from "react";
 import './App.css';
 
 export default function App() {
   return (
 <div className="App">
+
+  <div className="container"> 
+
       <h1> Weather App </h1>
 <footer> This is project was coded by <a href="https://www.instagram.com/_andreacodes/" target="_blank">  Andrea </a> and is open-source on {" "}
 
@@ -13,6 +15,7 @@ export default function App() {
          GitHub
         </a>
         </footer>
+        </div>
     </div>
   );
 }
