c858564086bd931961ad2c0675a513999eb9054e
Andrea Dubuc <dubuc.andrea@gmail.com>
2022-01-11 23:25:58 -0500

    Created weather component
    Started building weather app structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
+import Weather from "./Weather";
 import './App.css';
+
 
 export default function App() {
   return (
@@ -8,6 +10,7 @@ export default function App() {
   <div className="container"> 
 
       <h1> Weather App </h1>
+      <Weather/>
 <footer> This is project was coded by <a href="https://www.instagram.com/_andreacodes/" target="_blank">  Andrea </a> and is open-source on {" "}
 
       <a href="https://github.com/AndreaDubuc/my-react-app"
---
+++ src/Weather.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather (){
+    return(
+        <div className="Weather">
+            <form>
+                <div className="row">
+<div className="col-9">
+                <input 
+                type="search" 
+                placeholder="Enter a city..."
+                className="form-search"
+                />
+</div>
+<div className="col=3">
+                <input type="submit" value="search" className="btn btn-primary"/>
+</div>
+</div>
+            </form>
+            <h1> Seoul </h1>
+            <ul>
+                <li>Wednesday 09:00</li> 
+                <li> Mostly Cloudy</li>
+                </ul>
+<div className="row"> 
+<div className="col-6">
+<img className="Mainicon"
+src="https://www.kindpng.com/picc/m/209-2091974_partly-cloudy-clipart-image-with-rain-transparent-png.png" alt="Mostly Cloudy"/>
+6°C
+</div>
+<div className="col-6">
+<ul>
+<li>
+Precipitation: 15%
+</li>
+<li>
+Humidity:72%
+</li>
+<li>
+    Wind:13 km/h
+</li>
+
+</ul>
+
+</div>
+
+</div>
+
+
+
+
+
+
+            </div>
+    )
+}
