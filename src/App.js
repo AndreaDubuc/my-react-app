92bdc50a50f373ed19c33fa387b94ffd3a171557
Andrea Dubuc <dubuc.andrea@gmail.com>
2022-01-12 21:02:44 -0500

    Changes to project structure

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ export default function App() {
 
       <h1> Weather App </h1>
       <Weather/>
-<footer> This is project was coded by <a href="https://www.instagram.com/_andreacodes/" target="_blank">  Andrea </a> and is open-source on {" "}
+<footer> Coded by <a href="https://www.instagram.com/_andreacodes/" target="_blank">  Andrea </a> and is open-source on {" "}
 
       <a href="https://github.com/AndreaDubuc/my-react-app"
       target="_blank">
-         GitHub
+         GitHub 2022
         </a>
         </footer>
         </div>
--- src/Weather.js
+++ src/Weather.js
@@ -4,54 +4,105 @@ import "./Weather.css";
 export default function Weather (){
     return(
         <div className="Weather">
-            <form>
-                <div className="row">
-<div className="col-9">
-                <input 
-                type="search" 
+            <div className="search-form">
+            <div className="row">
+             <form className="col-6">
+               <input 
+                type="text" 
                 placeholder="Enter a city..."
                 className="form-search"
-                />
-</div>
-<div className="col=3">
-                <input type="submit" value="search" className="btn btn-primary"/>
-</div>
-</div>
-            </form>
-            <h1> Seoul </h1>
-            <ul>
-                <li>Wednesday 09:00</li> 
-                <li> Mostly Cloudy</li>
-                </ul>
-<div className="row"> 
-<div className="col-6">
-<img className="Mainicon"
-src="https://www.kindpng.com/picc/m/209-2091974_partly-cloudy-clipart-image-with-rain-transparent-png.png" alt="Mostly Cloudy"/>
-6°C
-</div>
-<div className="col-6">
-<ul>
-<li>
-Precipitation: 15%
-</li>
-<li>
-Humidity:72%
-</li>
-<li>
-    Wind:13 km/h
-</li>
-
-</ul>
-
-</div>
-
-</div>
-
-
-
-
-
-
+                autoComplete="off"
+                />  
+         </form>
+         <button className="magnifying-glass col-3">
+             <i className=" fas fa-search-location"/>
+         </button>
             </div>
-    )
-}
+            </div>  
+            <div className="date-time"></div>
+            <div className="current-weather">
+<div className="row">
+    <img
+    src="img"
+    className="main-icon"
+    alt="current weather icon"/>
+</div>
+<h1>
+    <span className="temp-value">--</span>
+    <span>
+        <button className="btnCelsius">
+            °C|
+        </button>
+        <button className="btnFahrenheit">
+            °F
+        </button>
+    </span>
+    </h1>
+    <div className="row">
+        <h2 className="location">loading...</h2>
+    </div>
+    </div>
+    <div className="weather-description">
+<div className="row">
+<div className="col max-min deg"><strong>--</strong></div>
+            <div className="col feels-like deg"><strong>--</strong></div>
+            <div className="col humidity deg"><strong>--</strong></div>
+            <div className="col wind deg"><strong>--</strong></div>
+</div>
+<div className="row">
+<div className="col">Max/Min</div>
+<div className="col">Precipitation</div>
+<div className="col">Humidity</div>
+<div className="col">Wind</div>
+</div>
+</div>
+<div className="temp-hours row">
+<div className="col">
+    <div className="row">
+        <div className="col"></div>
+    </div>
+<div className="row">
+
+    <div className="col">
+        <img
+    src="img"
+    className="main-icon"
+    alt="current weather icon"/>
+    </div>
+</div>
+<div className="row">
+<div className="col deg-hour"> <strong></strong></div>
+</div>
+</div>
+</div>
+
+<div className="temp-week-days row">
+    <div className="col">
+        <div className="row">
+            <div className="col"></div>
+        </div>
+        <div className="row">
+            <div className="col">
+<img
+    src="img"
+    className="main-icon"
+    alt="current weather icon"/>
+                
+            </div>
+        </div>
+<div className="row">
+              <div className="col deg-days"><strong></strong></div>
+            </div>  
+          </div>  
+        </div>
+      </div>
+    );
+  }
+
+
+    
+                 
+
+                
+
+
