90cc3a440d96ce71974c3c934985d2af4914e28a
Andrea Dubuc <dubuc.andrea@gmail.com>
2022-01-15 00:33:02 -0500

    refactore/renamed/deleted several components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import React from "react";
-import Weather from "./Weather";
-import './App.css';
-import Footer from "./Footer";
 
 
 
-export default function App() {
-  return (
-<div className="App">
-  <div className="Container"> 
-<Weather defaultCity="Cairo"/>
-<Footer></Footer>
-</div>
-    </div>
-  );
-}
+
+
 
 
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
-import './App.css';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
