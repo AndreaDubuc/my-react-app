cadf2ce501c462f9c9eabc9f4ecf7be66ab64565
Andrea Dubuc <dubuc.andrea@gmail.com>
2022-01-18 21:21:47 -0500

    changes to the CSS for the units

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,85 +1,80 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import "./CurrentWeather.css";
 import WeatherIcon from "./WeatherIcon";
 
-export default function CurrentWeather(props){
-const [unit, setUnit]= useState("celsius");
+export default function CurrentWeather(props) {
+  const [unit, setUnit] = useState("celsius");
 
-    function showFahrenheit(event){
-        event.preventDefault();
-        setUnit("fahrenheit");
-    }
-    function showCelsius(event){
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  function showCelsius(event) {
     event.preventDefault();
     setUnit("celsius");
-    }
-    function fahrenheit(){
-        return (props.celsius * 9) / 5 + 32;
-    }
-    if (unit === "celsius"){
-        return(
-          <div className="col">
-            <div className="row">
-              <div className="col d-flex flex-row justify-content-end">
-                      <h1
-                    id="temperature">{Math.round(props.celsius)}
-                </h1>
-                
-                  <div className="units my-auto">
-          <span className="C" id="celsius-link">
-            °C
-          </span>{" "}
-          |
-          <span className="F" id="fahrenheit-link">
-            <a href="/" onClick={showFahrenheit}>
-              °F
-            </a>
-          </span>
-        </div>
-        
-              </div>
-            
-            </div>
-            
-            <div className="row">
-          <div>
-          <WeatherIcon code={props.data}/>
-        </div>
-       
+  }
+  function fahrenheit() {
+    return (props.celsius * 9) / 5 + 32;
+  }
+  if (unit === "celsius") {
+    return (
+      <div className="col">
+        <div className="row">
+          <div className="col d-flex flex-row justify-content-end">
+            <h1 id="temperature">{Math.round(props.celsius)}</h1>
+
+            <div className="units my-auto">
+              <span className="C active" id="celsius-link">
+                °C
+              </span>{" "}
+              |
+              <a
+                href="/"
+                onClick={showFahrenheit}
+                className="F unactive"
+                id="fahrenheit-link"
+              >
+                °F
+              </a>
+              {" "}
             </div>
           </div>
+        </div>
+
+        <div className="row">
+          <div>
+            <WeatherIcon code={props.data} />
+          </div>
+        </div>
+      </div>
     );
   } else {
-    return(
-          <div className="col">
-            <div className="row">
-              <div className="col d-flex flex-row justify-content-end">
-                      <h1>
-                    <span id="temperature">{Math.round(fahrenheit())}</span>
-                </h1>
-                   
-                  <div className="units my-auto">
-          <span className="C" id="celsius-link">
-            <a href="/" onClick={showCelsius}>
-             °C 
-            </a>
-          </span>{" "}
-          |
-          <span className="F" id="fahrenheit-link">
-              °F
-          </span>
-        </div>
-              </div>
-            </div>
-            <div className="row">
-          <div>
-          <WeatherIcon code={props.data}/>
-        </div>
-       
+    return (
+      <div className="col">
+        <div className="row">
+          <div className="col d-flex flex-row justify-content-end">
+            <h1>
+              <span id="temperature">{Math.round(fahrenheit())}</span>
+            </h1>
+
+            <div className="units my-auto">
+              <a href="/" onClick={showCelsius} className="C unactive" id="celsius-link">
+                °C
+              </a>{" "}
+              |
+              <span className="F active" id="fahrenheit-link">
+                °F
+              </span>
             </div>
           </div>
-      
+        </div>
+        <div className="row">
+          <div>
+            <WeatherIcon code={props.data} />
+          </div>
+        </div>
+      </div>
     );
   }
 }
